4b16d90a315adcdca473ad7a1704a58e2d994bd9
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-26 21:01:33 +0300

    register tickit done

--- myFrontend/src/components/Register.js
+++ myFrontend/src/components/Register.js
@@ -10,17 +10,23 @@ export default function Register(props) {
     const [country, setCountry] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState(""); 
-    const [statas, setStatas] = useState(); 
+    const [statas, setStatas] = useState(false); 
+    const [statas1, setStatas1] = useState(false); 
     
     const cheakRegiste = () => {
         
         const newUser={ firstName:firstName, lastName:lastName, age:age, country:country, email:email, password:password};
         axios.post(`http://localhost:5000/users`,newUser)
             .then((response) => {
-                //setStatas(true)
-                if(response.firstName!==""){
+            if(response.data._id){
+                console.log()
                     setStatas(true)
-                }
+                    setStatas1(false)
+            }
+            else{
+                setStatas1(true)
+                setStatas(false)
+            }
             })
             .catch((err)=>{
                console.log("error")
@@ -30,6 +36,7 @@ export default function Register(props) {
 
     return (
         <div className="Register">
+            <p>Register :</p>
             <input className="RegisterInput" type="text" placeholder="firstName" onChange={(e) => {
                 setFirstName(e.target.value);
             }} />
@@ -50,7 +57,8 @@ export default function Register(props) {
             }} />
 
             <button className="RegisterButton" onClick={cheakRegiste}>Register</button>
-            {statas?<div class="trueRegiste">The user has been created successfully</div> :<div class="falseRegiste">Error happened while register, please try again</div>}
+            {statas?<div class="trueRegiste">The user has been created successfully</div> :""}
+            {statas1?<div class="falseRegiste">Error happened while register, please try again</div> :""}
                 
             <p style={{ cursor: 'pointer' }} onClick={() => { history.push('/'); }}> Home </p>
         </div>
